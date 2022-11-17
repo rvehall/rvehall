e7c2dca0b40c791507f32ca7a43324e1fe180ee2
rvehall <rvehall@gmail.com>
2022-11-17 16:39:51 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+let w = 0;
+let h = 0;
+let numLines = 0;
+const input = document.getElementById("command");
+let background = "<div class='tilde'>~</div>";
+
+
+
+
+getWindowSize();
+repeatTilde();
+onresize = (event) => {
+    getWindowSize();
+    repeatTilde();
+}
+
+function getWindowSize() {
+    w = window.innerWidth;
+    h = window.innerHeight;
+    document.getElementById("windowSize").innerText = `${w} x ${h}`;
+}
+
+function repeatTilde() {
+    let el = document.getElementById('content');
+    let lh = parseInt(document.defaultView.getComputedStyle(el, null).getPropertyValue("font-size").replace("px", "")) * 1.14;
+    numLines = h / lh - 6;
+    let result = background.repeat(numLines);
+    document.getElementById("background").innerHTML = result;
+}
+
+document.addEventListener("keydown", (event) => document.getElementById("command").focus())
+input.addEventListener("keypress", (event) => {
+    document.getElementById("command").focus();
+    if (event.key === "Enter") {
+        event.preventDefault();
+        switch (event.target.value.toLowerCase()) {
+            case 'linkedin':
+            case ':linkedin':
+                window.open('https://www.linkedin.com/in/rvehall/', '_blank');
+                break;
+            case 'code':
+            case ':code':
+                window.open('https://github.com/rvehall/', '_blank');
+                break;
+            default:
+                alert("Please enter a valid request")
+        }
+    }
+});
